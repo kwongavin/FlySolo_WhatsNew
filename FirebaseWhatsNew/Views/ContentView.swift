a32a8159f62612b5818fa6e6406bff01f1873a6b
Gavin Kwon <kwongavin@gmail.com>
2023-10-04 22:23:14 -0700

    - Image changed

--- FirebaseWhatsNew/Views/ContentView.swift
+++ FirebaseWhatsNew/Views/ContentView.swift
@@ -36,19 +36,19 @@ struct ContentView: View {
                                     .frame(height: 100)
                             }
                             VStack(spacing: geo.size.width*0.03) {
-                                Text("Finally, Available on Apple App Store!")
+                                Text(article.whatsNewTitle ?? "")
                                     .bold()
-                                Text("Thanks for installing FlySolo. Now, spread the word to your musician buds and drop us a stellar review! Your positive feedback will make us do the happy dance. 🕺💃") // article.whatsNewText
+                                Text(article.whatsNewText)
                             }
-                            .padding(.top)
-                            .frame(maxHeight: .infinity, alignment: .top)
+                            .padding(.top, 10)
+//                            .frame(maxHeight: .infinity, alignment: .top)
                             .font(Font.custom("Avenir Roman", size: geo.size.width*0.043))
                         }
                         .padding()
                     }
                 }
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-                .background(Color.blue.opacity(0.1))
+                .background(Color.blue.opacity(0.05))
             }
         }
         .frame(width: screenWidth, height: screenHeight*0.4)
